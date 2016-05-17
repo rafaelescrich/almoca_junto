dfdc0f7cf6320e9a9d111afaed917aefb0c55e03
Rafael Escrich <rafaelescrich@gmail.com>
2016-05-17 11:17:00 -0300

    added group and tests

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+require 'rails_helper'
+
+RSpec.describe Group, :type => :model do
+  before do
+    @group = Group.new(name: "NUTE")
+  end
+
+
+  subject { @group }
+
+  describe "when name is not present" do
+    before { @group.name = " " }
+    it { should_not be_valid }
+  end
+end
